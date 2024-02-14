60f510bb42b9128a5378d382a855e40e78d597c9
alymahryn <alymahryn@gmail.com>
2024-02-14 22:35:47 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import random
 import tkinter as tk
+from tkinter import ttk
 from tkinter import PhotoImage
 
 randomNummber = random.randint(1, 20)
@@ -50,34 +51,52 @@ img = PhotoImage(file='logo.png')
 img_label = tk.Label(root, image=img)
 img_label.pack(pady=(10, 0))
 
-preview_label = tk.Label(root, text=randomNummber, font=(None, 8, "bold"), fg="grey")
+#Random Number Preview
+preview_label = tk.Label(root, text=randomNummber,
+                         font=(None, 8,"bold"),
+                         fg="grey")
 preview_label.pack()
 
-high_score_label = tk.Label(root, text=f"High Score: {score if score != float('inf') else '0'}", font=(None, 12, "bold"), fg="green")
-high_score_label.pack()
+#Scoreboard
+score_board = tk.Frame(root)
 
-attempts_label = tk.Label(root, text=f"Attempts: {attempts}", font=(None, 12, "bold"), fg="orange")
-attempts_label.pack()
+high_score_label = tk.Label(master=score_board, 
+                            text=f"High Score: {score if score != float('inf') else '0'}",
+                            font=(None, 12, "bold"),
+                            fg="green")
+attempts_label = tk.Label(master=score_board, 
+                          text=f"Attempts: {attempts}",
+                          font=(None, 12, "bold"),
+                          fg="orange")
+high_score_label.pack(side="left")
+attempts_label.pack(side="right")
+score_board.pack()
 
-guess_label = tk.Label(root, text="Guess a Number Between 1 & 20" , font=(None, 16, "bold"))
+#Label
+guess_label = tk.Label(root, text="Guess a Number Between 1 & 20",
+                       font=(None, 16, "bold"))
 guess_label.pack()
 
+#Input
 guess_entry = tk.Entry(root)
 guess_entry.pack()
 
-button_frame = tk.Frame(root)
-button_frame.pack()
-
+#Message
 result_label = tk.Label(root, text="")
 result_label.pack(pady=(10, 0))
 
-check_button = tk.Button(root, text="Check", command=check_guess)
-check_button.pack(side="left", padx=(50, 0))
+#Buttons
+button_frame = tk.Frame(root)
 
-check_button = tk.Button(root, text="Reset", command=reset_game)
-check_button.pack(side="right", padx=(0, 50))
+check_button = tk.Button(master=button_frame, text="Check", command=check_guess)
+reset_button = tk.Button(master=button_frame, text="Reset", command=reset_game)
+
+check_button.pack(side="left")
+reset_button.pack(side="left")
+
+button_frame.pack()
 
 
-
+#loop
 root.mainloop()
 
